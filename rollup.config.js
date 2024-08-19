a02b5ad10eb78140be091566b9c1fd28cb588fe8
Saikat Das <saikatdas40g@gmail.com>
2024-08-19 20:42:19 +0530

    chore: Update package.json and rollup.config.js for Rollup plugins

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,21 @@ import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
 import babel from "@rollup/plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
+import replace from "@rollup/plugin-replace";
 import commonjs from "@rollup/plugin-commonjs";
 import { defineConfig } from "rollup";
+
+function removeUseClientDirective() {
+  return {
+    name: 'remove-use-client-directive',
+    transform(code, id) {
+      if (id.includes('node_modules/@mui/styled-engine/GlobalStyles')) {
+        return code.replace(/'use client';/g, '');
+      }
+    },
+  };
+}
 
 export default defineConfig({
   input: "src/index.ts",
@@ -21,6 +34,11 @@ export default defineConfig({
   ],
   external: ["react", "react-dom"],
   plugins: [
+    terser(),
+    replace({
+      preventAssignment: true,
+      "use client": "",
+    }),
     typescript({ tsconfig: "tsconfig.json" }),
     postcss({
       extract: true,

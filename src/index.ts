e915d45d657cbb977a83ce8237ef15163c3057bb
Saikat Das <saikatdas40g@gmail.com>
2024-08-27 08:28:38 +0530

    chore: Bump package.json version to 1.1.3

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from "./components/Button/Button";
+export * from "./components/Button/ButtonGroup";
 export * from "./components/Input/Input";
 export * from "./components/Select/SelectInput";
 export * from "./components/FormWrapper/FormWrapper";

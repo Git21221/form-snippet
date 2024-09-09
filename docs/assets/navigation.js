131f6ed3d44ad60ed2eb48a60ee8367627c078b0
Saikat Das <saikatdas40g@gmail.com>
2024-09-09 13:11:53 +0530

    wdadaw wa dAWSZ

--- docs/assets/navigation.js
+++ docs/assets/navigation.js
@@ -1,1 +1,2 @@
-window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAACoXQywrCMBAF0H+ZdfBRfGCWIoo7F4ILcVFspEWbGdIJWsR/FyJt1abN+t57yOT4BFYPBglLy4x6ZZASvOudQSpAAMWcggQuSRVDT2WQcn4DAddMJyCj0WI+nkYvUatbTZa9WJOEDFfal6T+iToICZ+HN/OL1WfOUFcn/e5nk9a0urmbqBpBamPQUrfj4h5kjSY/mJhIGR/yFfcg7u99cxe0hqc3os14BCYCAAA="
+window.navigationData =
+  "data:application/octet-stream;base64,H4sIAAAAAAAACoXQywrCMBAF0H+ZdfBRfGCWIoo7F4ILcVFspEWbGdIJWsR/FyJt1abN+t57yOT4BFYPBglLy4x6ZZASvOudQSpAAMWcggQuSRVDT2WQcn4DAddMJyCj0WI+nkYvUatbTZa9WJOEDFfal6T+iToICZ+HN/OL1WfOUFcn/e5nk9a0urmbqBpBamPQUrfj4h5kjSY/mJhIGR/yFfcg7u99cxe0hqc3os14BCYCAAA=";
